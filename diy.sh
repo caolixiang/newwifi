fea11a89c34f40018c64b3b31e3e499287617f18
caolixiang <caolixiang@gmail.com>
2020-04-20 15:00:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Lienol/openwrt-package package/Lienol
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
